6f0cceac480c53d574d702cdbd7c0fbb4ad0a93b
Ryan Slater <ryan.j.slater.2@gmail.com>
2021-06-02 17:21:07 -0400

    redid dincing

--- Cogs/UserCommands.py
+++ Cogs/UserCommands.py
@@ -1,10 +1,10 @@
-from time import time
+from datetime import datetime, timedelta
+from time import time, sleep
 
 import discord
 import pandas as pd
 from discord.ext import commands
-from discord_components import (Button, ButtonStyle, DiscordComponents,
-                                InteractionType)
+from discord_components import Button, ButtonStyle, InteractionType
 from utils import *
 
 
@@ -15,12 +15,21 @@ def setup(bot):
 class UserCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.dumbasses = pd.read_csv('dumbasses.csv')
+
+        # Dumbass list
+        self.dumbass_list = pd.read_csv('dumbasses.csv')
+        self.dumbass_list_indicies = {}
+        self.dumbass_list_components = []
+        self.dinc_polls = {}
+
+        # Calculator
+        self.calc_strings = {}
+        self.calculator_components = []
 
     @commands.command()
     async def poll(self, ctx, question, *options: str):
         # Delete sender's message
-        await ctx.channel.purge(limit=1)
+        await ctx.message.delete()
 
         # Need between 2 and 10 options for a poll
         if not (1 < len(options) <= 10):
@@ -67,65 +76,161 @@ class UserCommands(commands.Cog):
         # 7 8 9 +
         # 4 5 6 -
         # 1 2 3 *
-        # . 0 = /
+        # . 0 < /
+        # C ( ) =
         empty_button = Button(style=ButtonStyle.gray, label=' ')
-        components = [[empty_button, empty_button, empty_button, empty_button] for _ in range(4)]
-        components.append([empty_button, empty_button])
+        self.calculator_components = [[empty_button, empty_button, empty_button, empty_button] for _ in range(5)]
 
         # Numbers
-        components[0][0] = Button(style=ButtonStyle.blue, label='7')
-        components[0][1] = Button(style=ButtonStyle.blue, label='8')
-        components[0][2] = Button(style=ButtonStyle.blue, label='9')
-        components[1][0] = Button(style=ButtonStyle.blue, label='4')
-        components[1][1] = Button(style=ButtonStyle.blue, label='5')
-        components[1][2] = Button(style=ButtonStyle.blue, label='6')
-        components[2][0] = Button(style=ButtonStyle.blue, label='1')
-        components[2][1] = Button(style=ButtonStyle.blue, label='2')
-        components[2][2] = Button(style=ButtonStyle.blue, label='3')
-        components[3][1] = Button(style=ButtonStyle.blue, label='0')
+        self.calculator_components[0][0] = Button(style=ButtonStyle.blue, label='7')
+        self.calculator_components[0][1] = Button(style=ButtonStyle.blue, label='8')
+        self.calculator_components[0][2] = Button(style=ButtonStyle.blue, label='9')
+        self.calculator_components[1][0] = Button(style=ButtonStyle.blue, label='4')
+        self.calculator_components[1][1] = Button(style=ButtonStyle.blue, label='5')
+        self.calculator_components[1][2] = Button(style=ButtonStyle.blue, label='6')
+        self.calculator_components[2][0] = Button(style=ButtonStyle.blue, label='1')
+        self.calculator_components[2][1] = Button(style=ButtonStyle.blue, label='2')
+        self.calculator_components[2][2] = Button(style=ButtonStyle.blue, label='3')
+        self.calculator_components[3][1] = Button(style=ButtonStyle.blue, label='0')
 
         # Other functions
-        components[3][0] = Button(style=ButtonStyle.gray, label='.')
-        components[3][2] = Button(style=ButtonStyle.green, label='=')
-        components[0][3] = Button(style=ButtonStyle.red, label='+')
-        components[1][3] = Button(style=ButtonStyle.red, label='-')
-        components[2][3] = Button(style=ButtonStyle.red, label='*')
-        components[3][3] = Button(style=ButtonStyle.red, label='/')
-        components[4][0] = Button(style=ButtonStyle.gray, label='c')
-        components[4][1] = Button(style=ButtonStyle.gray, label='<-')
+        self.calculator_components[0][3] = Button(style=ButtonStyle.red, label='+')
+        self.calculator_components[1][3] = Button(style=ButtonStyle.red, label='-')
+        self.calculator_components[2][3] = Button(style=ButtonStyle.red, label='*')
 
-        # Creating print line
-        calc_string = ''
-        formatted_calc_string = '`' + (34 - len(calc_string)) * ' ' + calc_string + '`'
+        self.calculator_components[3][0] = Button(style=ButtonStyle.gray, label='.')
+        self.calculator_components[3][2] = Button(style=ButtonStyle.gray, label='<-')
+        self.calculator_components[3][3] = Button(style=ButtonStyle.red, label='/')
+
+        self.calculator_components[4][0] = Button(style=ButtonStyle.gray, label='C')
+        self.calculator_components[4][1] = Button(style=ButtonStyle.gray, label='(')
+        self.calculator_components[4][2] = Button(style=ButtonStyle.gray, label=')')
+        self.calculator_components[4][3] = Button(style=ButtonStyle.green, label='=')
 
         # Send message
-        await ctx.channel.send(formatted_calc_string, components=components)
+        message = await ctx.channel.send('`' + 34 * ' ' + '`', components=self.calculator_components)
+        self.calc_strings[message.id] = ''
 
-        # Update message
-        while True:
-            # interaction = await bot.wait_for("button_click", check=lambda i: i.component.label.startswith("WOW"))
-            res = await self.bot.wait_for("button_click")
+    @commands.Cog.listener()
+    async def on_button_click(self, res):
+        msg_id = res.message.id
 
-            # If number or decimal clicked, just add to string
-            if res.component.label in '.1234567890':
-                calc_string += res.component.label
+        # If for calculator
+        if msg_id in self.calc_strings.keys():
+            # Clear ERROR
+            if self.calc_strings[msg_id] == 'ERROR':
+                self.calc_strings[msg_id] = ''
+
+            # If number, decimal, or parenthesis clicked, just add to string
+            if res.component.label in '.1234567890()':
+                self.calc_strings[msg_id] += res.component.label
             # If other thing clicked, add it with space padding
             elif res.component.label in '+-*/':
-                calc_string += f' {res.component.label} '
+                self.calc_strings[msg_id] += f' {res.component.label} '
             # If <- pressed, delete last char
             elif res.component.label == '<-':
-                calc_string = calc_string[:-1]
+                self.calc_strings[msg_id] = self.calc_strings[msg_id][:-1]
             # If c pressed, clear
-            elif res.component.label == 'c':
-                calc_string = ''
+            elif res.component.label == 'C':
+                self.calc_strings[msg_id] = ''
             # If = pressed, do math
             else:
-                calc_string = str(eval(calc_string))
+                try:
+                    calc_list = self.calc_strings[msg_id].split(' ')
+                    for i in range(len(calc_list)):
+                        if calc_list[i] != '0':
+                            calc_list[i] = calc_list[i].lstrip('0')
+                    self.calc_strings[msg_id] = ' '.join(calc_list)
+                    if len(self.calc_strings[msg_id]) == 0:
+                        self.calc_strings[msg_id] = '0'
+                    self.calc_strings[msg_id] = str(round(eval(self.calc_strings[msg_id]), 15))
+
+                except (SyntaxError, ZeroDivisionError):
+                    self.calc_strings[msg_id] = 'ERROR'
 
             # Update calc string
-            formatted_calc_string = '`' + (34 - len(calc_string)) * ' ' + calc_string + '`'
+            formatted_calc_string = '`' + (34 - len(self.calc_strings[msg_id])) * ' ' + self.calc_strings[msg_id] + '`'
+            await res.respond(type=InteractionType.UpdateMessage, content=formatted_calc_string, components=self.calculator_components)
 
-            await res.respond(type=InteractionType.UpdateMessage, content=formatted_calc_string, components=components)
+        # If for dumbass display
+        elif msg_id in self.dumbass_list_indicies.keys():
+            # Initial value modification
+            if res.component.label == '⏪':
+                self.dumbass_list_indicies[msg_id] = self.dumbass_list_indicies[msg_id] - 10
+            elif res.component.label == '◀':
+                self.dumbass_list_indicies[msg_id] = self.dumbass_list_indicies[msg_id] - 1
+            elif res.component.label == '▶':
+                self.dumbass_list_indicies[msg_id] = self.dumbass_list_indicies[msg_id] + 1
+            elif res.component.label == '⏩':
+                self.dumbass_list_indicies[msg_id] = self.dumbass_list_indicies[msg_id] + 10
+            elif str(res.component.emoji) == '✖':
+                try:
+                    is_admin = res.user.permissions_in(res.message.channel).administrator
+                    if is_admin:
+                        self.dumbass_list = self.dumbass_list.drop(self.dumbass_list_indicies[msg_id])
+                        self.dumbass_list.to_csv('dumbasses.csv', index=False)
+                        self.dumbass_list_indicies[msg_id] -= 1
+                        await self._build_dumbass_leaderboard(res.message.guild)
+                    else:
+                        await res.respond(type=InteractionType.ChannelMessageWithSource, content='Only admins can delete dincs!')
+                except AttributeError:
+                    await res.respond(type=InteractionType.ChannelMessageWithSource, content='Only admins can delete dincs!')
+
+            # Keep index in range
+            if self.dumbass_list_indicies[msg_id] < 0:
+                self.dumbass_list_indicies[msg_id] += len(self.dumbass_list)
+            elif self.dumbass_list_indicies[msg_id] >= len(self.dumbass_list):
+                self.dumbass_list_indicies[msg_id] %= len(self.dumbass_list)
+
+            self.dumbass_list_components[0][2] = Button(style=ButtonStyle.red, label=f'{self.dumbass_list_indicies[msg_id] + 1} / {len(self.dumbass_list)}', emoji='✖')
+            await res.respond(type=InteractionType.UpdateMessage, components=self.dumbass_list_components, embed=await self._dumbass_embed(res.message.guild, self.dumbass_list_indicies[msg_id]))
+
+        # If for dinc poll
+        elif msg_id in self.dinc_polls.keys():
+            target = await get_member(res.message.guild, self.dinc_polls[msg_id][0])
+            target = target.id
+
+            # If yes vote
+            if str(res.component.emoji) == '✔':
+                # If target votes yes, approve
+                if res.user.id == target:
+                    # Add to csv and save, delete poll
+                    reason = ' '.join(res.message.content.split(' ')[3:])[:-1]
+                    await res.message.delete()
+                    self.dumbass_list = self.dumbass_list.append({'id': target, 'reason': reason, 'timestamp': time()}, ignore_index=True)
+                    self.dumbass_list.to_csv('dumbasses.csv', index=False)
+                    await self._build_dumbass_leaderboard(res.message.guild)
+                    del self.dinc_polls[msg_id]
+                    return
+                else:
+                    # Add to yes list
+                    if res.user.id not in self.dinc_polls[msg_id][1]:
+                        self.dinc_polls[msg_id][1].append(res.user.id)
+                    # Or remove from yes list
+                    else:
+                        self.dinc_polls[msg_id][1].remove(res.user.id)
+                    # Remove from no list
+                    if res.user.id in self.dinc_polls[msg_id][2]:
+                        self.dinc_polls[msg_id][2].remove(res.user.id)
+
+            # If no vote
+            elif str(res.component.emoji) == '✖':
+                if res.user.id != target:
+                    # Add to no list
+                    if res.user.id not in self.dinc_polls[msg_id][2]:
+                        self.dinc_polls[msg_id][2].append(res.user.id)
+                    # Or remove from no list
+                    else:
+                        self.dinc_polls[msg_id][2].remove(res.user.id)
+                    # Remove from yes list
+                    if res.user.id in self.dinc_polls[msg_id][1]:
+                        self.dinc_polls[msg_id][1].remove(res.user.id)
+                else:
+                    await res.respond(type=InteractionType.ChannelMessageWithSource, content="You can't vote no when you're getting dinc'd")
+
+            poll_components = [[Button(style=ButtonStyle.green, emoji='✔', label=f'[{len(self.dinc_polls[msg_id][1])}]'),
+                                Button(style=ButtonStyle.red, emoji='✖', label=f'[{len(self.dinc_polls[msg_id][2])}]')]]
+            await res.respond(type=InteractionType.UpdateMessage, content=res.message.content, components=poll_components)
 
     @commands.command(aliases=['dinc'])
     async def dumbass_increment(self, ctx, member_id='0', *, reason):
@@ -133,25 +238,60 @@ class UserCommands(commands.Cog):
         if member_id == '0':
             member_id = ctx.author.id
         member_id = member_id.strip('<@!>')
-
         if reason is None:
             await ctx.send('Rerun command with reason')
             return
 
-        # Add to csv and save
-        self.dumbasses = self.dumbasses.append({'id': member_id, 'reason': reason, 'timestamp': time()}, ignore_index=True)
-        self.dumbasses.to_csv('dumbasses.csv')
+        # If author is dincing themselves, approve
+        author = ctx.author
+        target = await get_member(ctx.guild, member_id)
+        if target.id == author.id:
+            # Add to csv and save
+            self.dumbass_list = self.dumbass_list.append({'id': member_id, 'reason': reason, 'timestamp': time()}, ignore_index=True)
+            self.dumbass_list.to_csv('dumbasses.csv', index=False)
 
-        # Make leaderboard
+            # Make leaderboard
+            await self._build_dumbass_leaderboard(ctx.guild)
+
+        # If author is dincing someone else
+        else:
+            POLL_TIME = 60
+            await ctx.message.delete()
+            # Make a 60s timed poll to get approval from members (ignoring the target). Author auto votes yes
+            poll_components = [[Button(style=ButtonStyle.green, emoji='✔', label=f'[1]'),
+                                Button(style=ButtonStyle.red, emoji='✖', label='[0]')]]
+            message = await ctx.channel.send(f'**[{str(timedelta(seconds=POLL_TIME))[2:]}]** Dinc {target.mention} for {reason}?', components=poll_components)
+            self.dinc_polls[message.id] = (target.id, [author.id], [])  # Target, vote yes, vote no
+            for timer in range(POLL_TIME - 1, -1, -1):
+                sleep(1)
+                await message.edit(content=f'**[{str(timedelta(seconds=timer))[2:]}]** Dinc {target.mention} for {reason}?', components=message.components)
+
+            # Process results of poll
+            # If more yes votes than no votes
+            if len(self.dinc_polls[message.id][1]) - 1 > len(self.dinc_polls[message.id][2]):
+                # If dinc with this reason not already recently added to dataframe
+                df = self.dumbass_list.tail(10)
+                for _, row in df.iterrows():
+                    if row['id'] == target.id and row['reason'] == reason:
+                        return
+
+                # Add to csv and save, delete poll
+                await message.delete()
+                self.dumbass_list = self.dumbass_list.append({'id': target.id, 'reason': reason, 'timestamp': time()}, ignore_index=True)
+                self.dumbass_list.to_csv('dumbasses.csv', index=False)
+                await self._build_dumbass_leaderboard(message.guild)
+                del self.dinc_polls[message.id]
+
+    async def _build_dumbass_leaderboard(self, guild):
         # Check if category exists. If not, make it
         category = None
         category_name = 'Top Dumbasses'
-        for c in ctx.guild.categories:
+        for c in guild.categories:
             if c.name == category_name:
                 category = c
                 break
         if category is None:
-            category = await ctx.guild.create_category('Top Dumbasses', position=0)
+            category = await guild.create_category('Top Dumbasses', position=0)
             await category.edit(position=0)  # Ensuring it actually goes to the top
 
         # Delete every channel under it
@@ -159,41 +299,39 @@ class UserCommands(commands.Cog):
             await channel.delete()
 
         # Remake channels for leaderboard
-        scores = self.dumbasses['id'].value_counts()
+        scores = self.dumbass_list['id'].value_counts()
         for member_id, count in scores.iteritems():
-            member = await get_member(ctx.guild, member_id)
+            member = await get_member(guild, member_id)
             await category.create_text_channel(f'({count}) {member.display_name}')
+
+    async def _dumbass_embed(self, guild, index):
+        # Properties of dinc
+        member = await get_member(guild, self.dumbass_list.at[index, 'id'])
+        reason = str(self.dumbass_list.at[index, 'reason'])
+        if reason == 'nan':
+            reason = 'Unknown reason'
+        timestamp = self.dumbass_list.at[index, 'timestamp']
+        try:
+            timestamp = datetime.utcfromtimestamp(timestamp).strftime('%c')
+        except ValueError:
+            timestamp = 'Unknown timestamp'
+
+        # Create the embed
+        embed = discord.Embed(title=timestamp, description=reason)
+        embed.set_author(name=member.display_name, icon_url=str(member.avatar_url))
+        return embed
 
     @commands.command()
     async def dumbasses(self, ctx):
-        # Button styles: blue, gray, green, grey, randomColor, red, URL
-        components = []
-        for i in range(5):
-            line = [Button(style=ButtonStyle.blue, label=str(i * j)) for j in range(5)]
-            components.append(line)
-        await ctx.channel.send(
-            'Lookie! Pretty buttons!',
-            components=components
-            # components=[
-            #     Button(style=ButtonStyle.blue, label="Blue"),
-            #     Button(style=ButtonStyle.red, label="Red"),
-            #     Button(style=ButtonStyle.URL, label="url", url="https://example.org"),
-            # ],
-        )
+        # Create menu
+        index = len(self.dumbass_list) - 1
+        self.dumbass_list_components = [[Button(style=ButtonStyle.green, label='⏪'),
+                                        Button(style=ButtonStyle.green, label='◀'),
+                                        Button(style=ButtonStyle.red, label=f'{index + 1} / {len(self.dumbass_list)}', emoji='✖'),
+                                        Button(style=ButtonStyle.green, label='▶'),
+                                        Button(style=ButtonStyle.green, label='⏩')]]
+        embed = await self._dumbass_embed(ctx.guild, index)
 
-        res = await self.bot.wait_for("button_click")
-        await res.respond(type=InteractionType.ChannelMessageWithSource, content=f'{res.component.label} clicked')
-
-    # @commands.Cog.listener()
-    # async def on_button_click(self, res):
-    #     """
-        # Pong
-        # ChannelMessageWithSource         - replies just to you
-        # DeferredChannelMessageWithSource - replies to message with content, thinks forever
-        # DeferredUpdateMessage
-        # UpdateMessage                    - edits the message
-    #     """
-    #     # await res.respond(
-    #     #     type=InteractionType.ChannelMessageWithSource, content=f"{res.component.label} pressed"
-    #     # )
-    #     await res.respond(type=InteractionType.ChannelMessageWithSource, content=f'{res.component.label} clicked')
+        # Set index for message
+        message = await ctx.channel.send(embed=embed, components=self.dumbass_list_components)
+        self.dumbass_list_indicies[message.id] = index
--- README.md
+++ README.md
@@ -5,3 +5,5 @@ My own personal Discord bot
 Requires discord-py >= 1.7.2
 
 `conda install -c conda-forge discord.py`
+
+Uses [discord-components](https://devkiki7000.gitbook.io/discord-components/)
